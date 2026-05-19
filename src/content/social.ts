3cfb3febadb5e242298b0349376704b9c1cc3b92
Sebastián Sifontes Valentín <145695928+sebsv123@users.noreply.github.com>
2026-05-19 19:09:12 +0200

    feat: update social links to Sebastian Sifontes

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,7 +1,5 @@
 export const social = [
-  { url: "mailto:me@david-hckh.com", name: "mail" },
-  { url: "https://github.com/davidhckh", name: "github" },
-  { url: "https://www.linkedin.com/in/david-heckhoff/", name: "linkedin" },
-  { url: "https://x.com/DavidHckh", name: "x" },
-  //{ url: "https://www.instagram.com/davidhckh/", name: "instagram" },
+  { url: "mailto:ssifontes@valentinproteccionintegral.com", name: "mail" },
+  { url: "https://github.com/sebsv123", name: "github" },
+  { url: "https://www.linkedin.com/in/sebastian-sifontes-valentin-752665187/", name: "linkedin" },
 ] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
